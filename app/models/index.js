4631d521b394545e596a7b90b1c64d76174c0efa
aaronfeingold <ajfeingold88@gmail.com>
2021-05-25 00:12:31 -0500

    switching to pg

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require('../config/db.config.js');
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
+var pgp = require('pg-promise')
 const db = {}
 db.mongoose = mongoose
 db.url = dbConfig.url;
