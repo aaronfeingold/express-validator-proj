0131f9f1757036bb83eabb285c38af9f11a1d6dc
aaronfeingold <ajfeingold88@gmail.com>
2021-05-24 22:11:20 -0500

    added procfile, cleaned up server

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes/post.routes')(app)
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
